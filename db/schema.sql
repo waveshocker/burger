f3d9524280c19d3d9124efc68b62bba7173961f9
unknown <binhu10@gmail.com>
2020-04-26 15:50:35 -0400

    Set up initial DB

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS burger_db;
+-- Creates the "blogger" database --
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
